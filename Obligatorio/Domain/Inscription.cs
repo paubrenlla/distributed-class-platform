9fd362a05d25c5a29877ad19be07156deff2964e
Pau Brenlla <paula@anagram.dev>
2025-09-17 13:10:32 -0300

    feat: creacion de clase inscripcion y repositorio de la misma

---
+++ Obligatorio/Domain/Inscription.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Domain
+{
+    public enum InscriptionStatus
+    {
+        Active,
+        Cancelled,
+        Finished
+    }
+
+    public class Inscription
+    {
+        private static int _nextId = 1;
+        public int Id { get; private set; }
+        public User User { get; private set; }
+        public OnlineClass Class { get; private set; }
+        public DateTimeOffset EnrollmentDate { get; private set; }
+        public InscriptionStatus Status { get; private set; }
+
+        public Inscription(User user, OnlineClass onlineClass)
+        {
+            Id = _nextId++;
+            User = user ?? throw new ArgumentNullException(nameof(user));
+            Class = onlineClass ?? throw new ArgumentNullException(nameof(onlineClass));
+            EnrollmentDate = DateTimeOffset.Now;
+            Status = InscriptionStatus.Active;
+        }
+
+        public void Cancel()
+        {
+            Status = InscriptionStatus.Cancelled;
+        }
+    }
+}
